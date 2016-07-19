7fb3475c96304f163cb8f5fb41004cad445150a1
elemenka <elemenka@gmail.com>
2016-07-19 00:23:48 -0400

    attempt at cash register

--- week6/homework/temperature-converter/js/project.js
+++ week6/homework/temperature-converter/js/project.js
@@ -1,50 +1,70 @@
-$('button').onclick = updateFahrenheit;
+var rawTemp;
+var rawUnit;
+var convertedTemp;
+var convertedUnit;
 
-$('button').onclick = updateCelsius;
-
-$('celsius').value = 0;
-
-// initial value on page load
-updateFahrenheit();
-
-function updateFahrenheit() {
-  var c = $('celsius').value;
-  var f;
-  if ( (c == null) || (c == "") || (isNaN(c) ) )
-  {
-    f = "";
-  }
-  else
-  {
-    f = celsiusToFahrenheit(c);
-  }
-  $('fahrenheit').value = f;
+$('#submit').click(function(){
+rawTemp = parseInt($('#temp').val());
+rawUnit = $("input[type='radio']:checked").val();
+if(rawUnit === 'F'){
+  convertedUnit = 'C';
+  convertedTemp = (rawTemp - 32) / 1.8;
+  $('#output').html(convertedTem + convertedUnit);
 }
 
-function updateCelsius() {
-  var f = $('fahrenheit').value;
-  var c;
-  if ( (f == null) || (f == "") || (isNaN(f) ) )
-  {
-    c = "";
-  }
-  else
-  {
-    c = fahrenheitToCelsius(f);
-  }
-  $('celsius').value = c;
-}
 
-function myround(x) {
-  return Math.round(10 * x) / 10;
-}
 
-function celsiusToFahrenheit(c) {
-  var answer = c * 9 / 5 + 32;
-  return myround(answer);
-}
+});
 
-function fahrenheitToCelsius(f) {
-  var answer = (f - 32) * 5 / 9;
-  return myround(answer);
-}
+
+
+// $('button').onclick = updateFahrenheit;
+
+// $('button').onclick = updateCelsius;
+
+// $('celsius').value = 0;
+
+// // initial value on page load
+// updateFahrenheit();
+
+// function updateFahrenheit() {
+//   var c = $('celsius').value;
+//   var f;
+//   if ( (c == null) || (c == "") || (isNaN(c) ) )
+//   {
+//     f = "";
+//   }
+//   else
+//   {
+//     f = celsiusToFahrenheit(c);
+//   }
+//   $('fahrenheit').value = f;
+// }
+
+// function updateCelsius() {
+//   var f = $('fahrenheit').value;
+//   var c;
+//   if ( (f == null) || (f == "") || (isNaN(f) ) )
+//   {
+//     c = "";
+//   }
+//   else
+//   {
+//     c = fahrenheitToCelsius(f);
+//   }
+//   $('celsius').value = c;
+// }
+
+// function myround(x) {
+//   return Math.round(10 * x) / 10;
+// }
+
+// function celsiusToFahrenheit(c) {
+//   var answer = c * 9 / 5 + 32;
+//   return myround(answer);
+// }
+
+// function fahrenheitToCelsius(f) {
+//   var answer = (f - 32) * 5 / 9;
+//   return myround(answer);
+// }
